cf12eba48ef78c4a6de47ac5487e648e00fdf33b
John Nilsson <john@mini.(none)>
2011-06-06 03:22:43 +0200

    Small tweaks to Akka test

--- src/test/scala/kairos/AkkaFirstSteps.scala
+++ src/test/scala/kairos/AkkaFirstSteps.scala
@@ -7,14 +7,18 @@ import akka.testkit.TestKit
 import akka.util.duration._
 
 class AkkaFirstSteps extends WordSpec with ShouldMatchers with TestKit {
-	"A WriterActor"  should {
-	  "persist events sent to it" in {
-		  val writer = actorOf[WriterActor].start
-	    within (15 millis) {
-	    	writer ! SaveEvent("MyId","MyData")
-	    	expectMsg("Done!")
-	    	expectNoMsg
-	    }
-	  }
-	}
+
+  "A WriterActor" should {
+
+    "persist events sent to it" in {
+
+      val writer = actorOf[WriterActor].start
+
+      within(15 millis) {
+        writer ! SaveEvent(id = "MyId", data = "MyData")
+        expectMsg("Done!")
+        expectNoMsg
+      }
+    }
+  }
 }
